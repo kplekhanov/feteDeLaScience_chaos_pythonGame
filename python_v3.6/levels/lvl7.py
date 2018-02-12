6529b0cc7112a9106a8a7a3dcc8c8ad94db21dad
Speikus <plekhanov.kirill@hotmail.com>
2018-02-12 19:40:52 +0100

    python 3.6

---
+++ python_v3.6/levels/lvl7.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from canon import Canon
+from levels.base import BaseLevel
+
+
+class Level7(BaseLevel):
+    MAX_TIME = 99.9 * 60
+
+    def initialize(self, world):
+        world.canon = Canon(r=75, pos=[world.width/2, world.height], theta=-np.pi/2)
+        world.canon.shotQuantity = 6
+        world.showTrajectories = True
